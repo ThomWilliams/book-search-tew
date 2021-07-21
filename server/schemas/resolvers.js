10a9b97727a75f8ec1ba536c14b1ae2c95d09d67
Thom Williams <thomwilliams1990@gmail.com>
2021-07-21 23:36:28 +0100

    Resolver added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,97 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { Book, User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  // ADD RESOLVERS - SEE CONTROLLERS vs. 
+  // ADD RESOLVERS - SEE CONTROLLERS vs.
+  Query: {
+    user: async () => {
+      return User.find({});
+    },
+    book: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Book.find(params);
+    },
+  },
+  Mutation: {
+    // get a single user by either their id or their username
+    getSingleUser: async ({ user = null, params }, res) => {
+      const foundUser = await User.findOne({
+        $or: [
+          { _id: user ? user._id : params.id },
+          { username: params.username },
+        ],
+      });
+
+      if (!foundUser) {
+        return res
+          .status(400)
+          .json({ message: "Cannot find a user with this id!" });
+      }
+      res.json(foundUser);
+    },
+
+    // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+    createUser: async ({ body }, res) => {
+      const user = await User.create(body);
+
+      if (!user) {
+        return res.status(400).json({ message: "Something is wrong!" });
+      }
+      const token = signToken(user);
+      res.json({ token, user });
+    },
+
+    // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+    // {body} is destructured req.body
+    login: async ({ body }, res) => {
+      const user = await User.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
+      if (!user) {
+        return res.status(400).json({ message: "Can't find this user" });
+      }
+
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        return res.status(400).json({ message: "Wrong password!" });
+      }
+      const token = signToken(user);
+      res.json({ token, user });
+    },
+
+    // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+    // user comes from `req.user` created in the auth middleware function
+    saveBook: async ({ user, body }, res) => {
+      console.log(user);
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return res.json(updatedUser);
+      } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+      }
+    },
+    // remove a book from `savedBooks`
+    deleteBook: async ({ user, params }, res) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res
+          .status(404)
+          .json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
+    },
+  },
 };
 
 module.exports = resolvers;
