f4304d8ad51179a4764fcb77804667a588881719
Thom Williams <thomwilliams1990@gmail.com>
2021-07-21 08:37:44 +0100

    TypeDefs defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,45 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
 
-# ADD TYPE DEFS - SEE README LINE 91 + TYPE DEFS Vs MODELS IN EXAMPLES
+
+// # ADD TYPE DEFS - SEE README LINE 91 + TYPE DEFS Vs MODELS IN EXAMPLES
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [bookSchema]
+  }
+
+  type Book {
+    _id: ID!
+    authors: String!
+    description: String!
+    bookId: String!
+    image: String!
+    link: String!
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: [User]
+    book(_id: String): [Book]
+  }
+
+  type Mutation {
+    getSingleUser:(_id: String!, username: String!): User
+    createUser(_id: String!, username: String!, email: String!, password: String!): Auth
+    login(username: String!, email: String!): Auth
+    saveBook(_id: String!, bookId: String!): User
+    deleteBook:(_id: String!, bookId: String!): User
+  }
 
 `;
 
