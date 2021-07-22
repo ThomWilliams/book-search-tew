dd0c6a335d91e43680637e4395236d78415bdd90
Thom Williams <thomwilliams1990@gmail.com>
2021-07-22 21:30:09 +0100

    Client: SignupForm.js: Updated for Apollo

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -28,7 +28,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // Added by Thom for Apollo 
+      const [addUser, { error }] = useMutation(ADD_USER);
+      // const response = await createUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
 // import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+// import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
@@ -105,6 +105,7 @@ const SavedBooks = () => {
               </Card>
             );
           })}
+          
         </CardColumns>
       </Container>
     </>
